3d32cd786a0c01c9fadf5504c0e56aeb6f5fefc3
Mana Lyzor <manalyzor@gmail.com>
2020-10-08 12:43:07 +0330

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/main.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pt-1 w-screen min-h-screen bg-gray-300 mt-0 ">
+      <NavBar />
     </div>
   );
 }
